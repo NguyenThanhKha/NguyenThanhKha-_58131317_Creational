dab48f7c77aecefa346773d27684bb644f08bd89
Kha97 <nguyenthanhkharom@gmail.com>
2020-05-19 20:40:47 +0700

    v1.0

---
+++ src/nguyenthanhkha/_58131317_creational/NguyenThanhKha_58131317_Creational.java
@@ -0,0 +1,23 @@
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nguyenthanhkha._58131317_creational;
+
+/**
+ *
+ * @author HP
+ */
+public class NguyenThanhKha_58131317_Creational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
